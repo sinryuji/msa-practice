e5cc64449a0205330addac5b2734d07bf634b512
sinryuji <siler2474@gmail.com>
2025-02-06 18:32:41 +0900

    jwt authentication

---
+++ com.spring-cloud.eureka.client.auth/src/main/java/com/spring_cloud/eureka/client/auth/AuthService.java
@@ -0,0 +1,42 @@
+package com.spring_cloud.eureka.client.auth;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoder;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Date;
+
+@Service
+public class AuthService {
+
+    @Value("${spring.application.name}")
+    private String issuer;
+
+    @Value("${service.jwt.access-expiration}")
+    private Long expiration;
+
+    private final SecretKey secretKey;
+
+    public AuthService(@Value("${service.jwt.secret-key}") String secretKey) {
+        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+    }
+
+    public String createAccessToken(String userId) {
+        return Jwts.builder()
+                .claim("userId", userId)
+                .claim("role", "ADMIN")
+                .issuer(issuer)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secretKey)
+                .compact();
+    }
+}
+
---
+++ com.spring-cloud.eureka.client.gateway/src/main/java/com/spring_cloud/eureka/client/gateway/LocalJwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.spring_cloud.eureka.client.gateway;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+
+@Slf4j
+@Component
+public class LocalJwtAuthenticationFilter implements GlobalFilter {
+
+    @Value("${service.jwt.secret-key}")
+    private String secretKey;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+        if (path.equals("/auth/signIn")) {
+            return chain.filter(exchange);
+        }
+
+        String token = extractToken(exchange);
+
+        if (token == null || !validateToke(token)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    private String extractToken(ServerWebExchange exchange) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+        return null;
+    }
+
+    private boolean validateToke(String token) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .verifyWith(key)
+                    .build().parseSignedClaims(token);
+            log.info("#####payload :: " + claimsJws.getPayload().toString());
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
